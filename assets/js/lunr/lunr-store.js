0efc2169bfa0d636da7eaa9aa68acfad58bebc2c
sednabcn <ruperto.bonet@modelphysmat.com>
2025-04-04 14:02:13 +0000

    Deploying to gh-pages from @ sednabcn/ai-llm-blog@f6ee9d7c6bfc1dffaf4d1d5c7b4dfa6060abb34e 🚀

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "Exploring the Frontier: My Journey into AI and LLMs",
-        "excerpt":"Welcome to the AI Frontier Hello and welcome to my corner of the digital universe dedicated to artificial intelligence and large language models! I’m thrilled to embark on this journey of exploration and discovery with you. As we stand at the precipice of what many are calling the fourth industrial...","categories": ["AI Research"],
+        "excerpt":"Welcome to the AI Frontier Hello and welcome to my corner of the digital universe dedicated to artificial intelligence and large language models! I’m thrilled to embark on this journey of exploration and discovery with you. As we stand at the precipice of what many are calling the fourth industrial...","categories": [],
         "tags": ["artificial intelligence","large language models","LLM-HypatiaX","formula discovery"],
-        "url": "/ai-llm-blog/ai%20research/Exploring-the-Frontier-My-Journey-into-AI-and_LLMs/",
+        "url": "/ai-llm-blog/Exploring-the-Frontier-My-Journey-into-AI-and_LLMs/",
         "teaser": "/ai-llm-blog/assets/images/posts/ai-journey-header.jpg"
       }]
