71dc66b099e38ccad6ecfa902bdb941fdd3be196
sednabcn <ruperto.bonet@modelphysmat.com>
2025-04-29 09:57:26 +0000

    Deploying to gh-pages from @ sednabcn/ai-llm-blog@7d3fc16fe181ead5163e75dc650d6ecd2094eed0 🚀

--- assets/js/title-small-devices.js
+++ assets/js/title-small-devices.js
@@ -1,7 +1,9 @@
- document.addEventListener('DOMContentLoaded', function() {
-      const titleElement = document.querySelector('h1');
+document.addEventListener('DOMContentLoaded', function() {
+    const titleElement = document.querySelector('h1.blog-title');
+
+    if (!titleElement) return;
       
-      if (window.innerWidth <= 768) {  // Small device
+    if (window.innerWidth <= 768) {  // Small device
           if (titleElement) {
 	      titleElement.innerHTML = `
       <span class="title-part">AI, LLM &</span>
